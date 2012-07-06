9d69c6aba60b11cf8a0acdd457a6e51499a5654c
Derek P <derek@derekperez.com>
2012-07-05 22:42:40 -0700

    fixed a bug with bitwise length calculations for TCP send.

--- riemann/socket.js
+++ riemann/socket.js
@@ -45,9 +45,9 @@ tcpSocket.prototype.send = function(payload) {
   assert(Buffer.isBuffer(payload));
   var len = payload.length;
   var packet = new Buffer(len + 4);
-  packet[0] = len >>> 24;
-  packet[1] = len >>> 16;
-  packet[2] = len >>> 8;
+  packet[0] = len >>> 24 & 0xFF;
+  packet[1] = len >>> 16 & 0xFF;
+  packet[2] = len >>> 8  & 0xFF;
   packet[3] = len &   255;
   payload.copy(packet, 4, 0);
   this.socket.write(packet);
